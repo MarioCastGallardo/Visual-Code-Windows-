3547051f36a3ba5606641c025036dc88f61780b7
Mario Gallardo <macoktt@gmail.com>
2026-03-20 17:44:31 -0500

    Add files via upload

---
+++ Practica 3.8/Practica 3.8/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_3._8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 5; i++)
+
+            {
+                listBox1.Items.Add(i);
+            }
+        }
+    }
+}
