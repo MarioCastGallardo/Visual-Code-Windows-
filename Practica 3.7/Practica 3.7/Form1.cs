1d6db85d74cd7d05023b9c1bcd46f4285337d800
Mario Gallardo <macoktt@gmail.com>
2026-03-20 17:27:53 -0500

    Add files via upload

---
+++ Practica 3.7/Practica 3.7/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_3._7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int rangoinferior , rangosuperior, suma = 0;
+
+            rangoinferior = int.Parse(textBox1.Text);
+            rangosuperior = int.Parse(textBox2.Text);
+            for(int i = rangoinferior; i <= rangosuperior; i++) 
+
+            {
+                listBox1.Items.Add(i);  
+
+                suma += i;
+            }
+            textBox3.Text = suma.ToString();    
+        }
+    }
+}
