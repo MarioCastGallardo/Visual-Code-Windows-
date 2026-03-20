1b3c8d97aac0d528401bf5f4865f0e2ac894717a
Mario Gallardo <macoktt@gmail.com>
2026-03-19 22:15:01 -0500

    Add files via upload

---
+++ Practica 2.2/Practica 2.2/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_2._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sueldo, aum, nsue;
+            sueldo = double.Parse(textBox1.Text);
+            if (sueldo < 400000.00)
+            {
+                aum = sueldo * 0.15;
+                nsue= sueldo + aum;
+                textBox2.Text = nsue.ToString();
+            }
+        }
+    }
+}
