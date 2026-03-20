1efbebf612a40ef06c14e29652e3c2d38166389d
Mario Gallardo <macoktt@gmail.com>
2026-03-19 22:19:33 -0500

    Add files via upload

---
+++ Practica 2.9/Practica 2.9/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Practica_2._9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonEvaluar = new System.Windows.Forms.Button();
+            this.buttonBorrar = new System.Windows.Forms.Button();
+            this.buttonSalir = new System.Windows.Forms.Button();
+            this.textBoxEdad = new System.Windows.Forms.TextBox();
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(593, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "diagrama que pida la edad de una persona y determine si se trata de un niño (<12 " +
+    "años),un ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(648, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "adolescente (12-17 años), mayor de edad (18-60 años) o perteneciente a la tercera" +
+    " edad (>60 años)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(121, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Edad:";
+            // 
+            // buttonEvaluar
+            // 
+            this.buttonEvaluar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEvaluar.Location = new System.Drawing.Point(100, 361);
+            this.buttonEvaluar.Name = "buttonEvaluar";
+            this.buttonEvaluar.Size = new System.Drawing.Size(100, 30);
+            this.buttonEvaluar.TabIndex = 3;
+            this.buttonEvaluar.Text = "Evaluar ";
+            this.buttonEvaluar.UseVisualStyleBackColor = true;
+            this.buttonEvaluar.Click += new System.EventHandler(this.buttonEvaluar_Click);
+            // 
+            // buttonBorrar
+            // 
+            this.buttonBorrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBorrar.Location = new System.Drawing.Point(341, 361);
+            this.buttonBorrar.Name = "buttonBorrar";
+            this.buttonBorrar.Size = new System.Drawing.Size(97, 30);
+            this.buttonBorrar.TabIndex = 4;
+            this.buttonBorrar.Text = "Borrar";
+            this.buttonBorrar.UseVisualStyleBackColor = true;
+            this.buttonBorrar.Click += new System.EventHandler(this.buttonBorrar_Click);
+            // 
+            // buttonSalir
+            // 
+            this.buttonSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSalir.Location = new System.Drawing.Point(537, 361);
+            this.buttonSalir.Name = "buttonSalir";
+            this.buttonSalir.Size = new System.Drawing.Size(99, 30);
+            this.buttonSalir.TabIndex = 5;
+            this.buttonSalir.Text = "Salir ";
+            this.buttonSalir.UseVisualStyleBackColor = true;
+            this.buttonSalir.Click += new System.EventHandler(this.buttonSalir_Click);
+            // 
+            // textBoxEdad
+            // 
+            this.textBoxEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxEdad.Location = new System.Drawing.Point(100, 178);
+            this.textBoxEdad.Name = "textBoxEdad";
+            this.textBoxEdad.Size = new System.Drawing.Size(100, 28);
+            this.textBoxEdad.TabIndex = 6;
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.AutoSize = true;
+            this.labelResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResultado.Location = new System.Drawing.Point(96, 274);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(96, 22);
+            this.labelResultado.TabIndex = 7;
+            this.labelResultado.Text = "Resultado:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Practica_2._9.Properties.Resources.Captura_de_pantalla_2026_03_19_174343;
+            this.pictureBox1.Location = new System.Drawing.Point(419, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(333, 298);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 478);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelResultado);
+            this.Controls.Add(this.textBoxEdad);
+            this.Controls.Add(this.buttonSalir);
+            this.Controls.Add(this.buttonBorrar);
+            this.Controls.Add(this.buttonEvaluar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.Name = "Form1";
+            this.Text = "Hello world!";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonEvaluar;
+        private System.Windows.Forms.Button buttonBorrar;
+        private System.Windows.Forms.Button buttonSalir;
+        private System.Windows.Forms.TextBox textBoxEdad;
+        private System.Windows.Forms.Label labelResultado;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Practica 2.9/Practica 2.9/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_2._9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBorrar_Click(object sender, EventArgs e)
+        {
+            textBoxEdad.Text = "";
+        }
+
+        private void buttonSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonEvaluar_Click(object sender, EventArgs e)
+        {
+            {
+                int edad;
+
+                if (!int.TryParse(textBoxEdad.Text, out edad))
+                {
+                    MessageBox.Show("Ingresa una edad válida");
+                    return;
+                }
+
+                if (edad < 12)
+                {
+                    labelResultado.Text = "Es un Niño";
+                }
+                else if (edad < 18)
+                {
+                    labelResultado.Text = "Es un Adolescente";
+                }
+                else if (edad < 60)
+                {
+                    labelResultado.Text = "Es un Adulto";
+                }
+                else
+                {
+                    labelResultado.Text = "Es un Adulto Mayor";
+                }
+            }
+        }
+    }
+}
