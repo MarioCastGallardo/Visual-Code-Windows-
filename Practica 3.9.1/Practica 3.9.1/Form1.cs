1a5df3f2dff858a4241cd963122790b39db9cfd5
Mario Gallardo <macoktt@gmail.com>
2026-03-20 18:11:17 -0500

    Add files via upload

---
+++ Practica 3.9.1/Practica 3.9.1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_3._9._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+           textBox2.Text = "";
+            textBox3.Text = "";
+            listBox1.Items.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ulong numero = ulong.Parse(textBox1.Text);
+            ulong factorial = 1;
+            for (ulong i = 1; i <= numero; i++)
+            {
+              listBox1.Items.Add(i);
+                factorial = factorial * i;
+            }
+        textBox2.Text = numero.ToString();
+            textBox3.Text = factorial.ToString();
+        }
+
+    }
+}
