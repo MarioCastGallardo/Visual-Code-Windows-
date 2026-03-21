c760eb90e0b474cb85d31c78a713d6389c718d40
Mario Gallardo <macoktt@gmail.com>
2026-03-20 19:24:55 -0500

    Add files via upload

---
+++ Practica 3.12/Practica 3.12/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_3._12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            listBox1.Items.Clear();
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            double sueldo = double.Parse(textBox1.Text);
+            listBox1.Items.Add(sueldo);
+            textBox1.Clear();
+            textBox1.Focus();
+        }
+
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+            double nomina = 0;
+            foreach (var item in listBox1.Items)
+
+            {
+                nomina += Convert.ToDouble(item);
+            }
+            labelResultado.Text = "Nomina =" + nomina;
+        }
+    }
+}
